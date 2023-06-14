5cdf3d25aef7d7e8736fa0819cbdbff6cfcc5513
Tran Le Quan <75005806+Quan-GameDev@users.noreply.github.com>
2023-06-14 20:40:45 +0700

    Update June 14, 2023
    - Fix buttons
    - Update the server - client

--- Assets/Scripts/Managers/MainMenu.cs
+++ Assets/Scripts/Managers/MainMenu.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using Unity.Netcode;
 
 public class MainMenu : MonoBehaviour
 {
@@ -14,5 +15,20 @@ public class MainMenu : MonoBehaviour
    public void Quit(){
     Application.Quit();
     Debug.Log("Player has quit the game");
+   }
+
+    [SerializeField] private string gamePlaySceneName = "Gameplay";
+   public void StartHost(){
+        NetworkManager.Singleton.StartHost();
+        NetworkManager.Singleton.SceneManager.LoadScene(gamePlaySceneName, LoadSceneMode.Single);
+   }
+
+   public void StartServer(){
+        NetworkManager.Singleton.StartServer();
+        NetworkManager.Singleton.SceneManager.LoadScene(gamePlaySceneName, LoadSceneMode.Single);
+   }
+
+   public void StartClient(){
+        NetworkManager.Singleton.StartClient();
    }
 }
